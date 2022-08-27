ad0ce2076c72d657a17f0236ea47b87f8794557a
RenataMachado120 <112222882+RenataMachado120@users.noreply.github.com>
2022-08-27 15:31:48 -0300

    Adicionado codigo

---
+++ gerador de senhas.py
@@ -0,0 +1,25 @@
+import random
+import string
+def gerador_de_senhas(Len_pass = 8):
+    ascii_options = string.ascii_letters
+    number_options = string.digits
+    punt_options = string.punctuation
+    options = ascii_options + number_options + punt_options
+
+    password_user = ""
+
+    for i in range(0, Len_pass):
+        digit = random.choice(options)
+        password_user = password_user + digit
+
+    return password_user
+choice_user = input("Quantos digitos na senha?")
+
+if choice_user.isdigit():
+      choice_user = int(choice_user)
+else:
+      print("Erro")
+      quit()
+
+response = gerador_de_senhas(Len_pass = choice_user )
+print(f"Senha gerada:\n{response}")
